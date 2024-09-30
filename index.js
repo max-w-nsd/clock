ee126f65e01f3579b6edd834d43a0da923011eda
max-w-nsd <1038161@apps.nsd.org>
2024-09-30 11:34:20 -0700

    fifth commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var text = document.getElementById("time");
 
 function update() {
     var x = new Date();
-    var s = `${x.getHours}:${x.getMinutes}:${x.getSeconds}.${x.getMilliseconds}`
+    var s = `${x.getHours()}:${x.getMinutes()}:${x.getSeconds()}.${x.getMilliseconds()}`
     text.textContent = s;
     document.title = s;
 }
