2b30c3f350209df641d61c9b1b266d2f07179c3f
max-w-nsd <1038161@apps.nsd.org>
2024-10-01 18:04:42 +0000

    sixth commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-var text = document.getElementById("time");
+let text = document.getElementById("time");
 
 function update() {
-    var x = new Date();
-    var s = `${x.getHours()}:${x.getMinutes()}:${x.getSeconds()}.${x.getMilliseconds()}`
+    let x = new Date();
+    let s = `${pad2(x.getHours())}:${pad2(x.getMinutes())}:${pad2(x.getSeconds())}.${pad3(x.getMilliseconds())}`
     text.textContent = s;
     document.title = s;
+}
+
+function pad2(n) {
+    return ('00' + n).slice(-2);
+}
+
+function pad3(n) {
+    return ('000' + n).slice(-3);
 }
 
 setInterval(update);
